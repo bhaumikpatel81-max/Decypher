736a1e2ab4f88ccf4771f7630cb4af2c4e0a60ac
ByteBhaumik <bhaumik.patel81@gmail.com>
2026-05-01 11:13:16 +0530

    .bat remove

--- RunSetupAsAdmin.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-:: Launches Setup-AutoStart.ps1 with Administrator privileges
-powershell -Command "Start-Process powershell -ArgumentList '-ExecutionPolicy Bypass -File ""%~dp0Setup-AutoStart.ps1""' -Verb RunAs"
