8ead3103cfab920bc9c03f773ad61800b371b3cd
ByteBhaumik <bhaumik.patel81@gmail.com>
2026-05-06 16:20:11 +0530

    Working

--- angular-frontend/src/main.ts
+++ angular-frontend/src/main.ts
@@ -2,8 +2,4 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => {
-    const msg = err?.message || err?.toString() || JSON.stringify(err);
-    document.body.innerHTML = `<pre style="color:red;background:#fff;padding:24px;font-size:13px;white-space:pre-wrap;">${msg}</pre>`;
-    console.error(err);
-  });
+  .catch(err => console.error(err));
