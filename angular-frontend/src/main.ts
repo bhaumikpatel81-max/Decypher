fdcd874ddc1234b56f7787287bce5bdb6462bf64
ByteBhaumik <bhaumik.patel81@gmail.com>
2026-05-06 16:46:41 +0530

    main.ts

--- angular-frontend/src/main.ts
+++ angular-frontend/src/main.ts
@@ -1,5 +1,10 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 
+// Unregister any stale service workers that may be intercepting requests
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(regs => regs.forEach(r => r.unregister()));
+}
+
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
